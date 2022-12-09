2e19f2a1b78a0fb255aa8cc91480dec1875bd168
Huy Phan <pqhcan@gmail.com>
2022-12-09 16:49:15 +0700

    'xongbuoi8'

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-const express = require ("express")
-const app = express ()
-app.use(express.json())
-app.use(express.static("."))
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.static("."));
 
-app.listen(8080)
+app.listen(8080);
 
-const { PrismaClient}  =require('@prisma/client')
-const prisma = new PrismaClient()  //tuong tu const model = initial-Model()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient(); //tuong tu const model = initial-Model()
 
+// app.get("/demo", async (req, res) => {
+//   let data = await prisma.fodd.findMany({
+//     // where:{
+//     //     user_id:4,
+//     // }
+//     // include:{
+//     //     food_type: true
+//     // }
+//     where: {
+//       food_name: {
+//         contains: "w",
+//       },
+//     },
+//   });
+//   res.send(data);
+// });
+app.get("/demo/:id", async (req, res) => {
+    let {id} = req.params
+  let data = await prisma.fodd.findMany({
+    include: {
+      food_type: true,
+    },
+    where:{
+        food_id:Number(id) //chú ý kiểu dữ liệu truyền vào đúng với datastyle model đã khai báo
+    }
+  });
+  res.send(data);
+});
 
-app.get("/demo",async(req,res)=>{
-    let data = await prisma.user.findMany()
-    res.send("hello")
-})
+//create,update
 
-
+app.post("/createFood", async (req, res) => {
+  let { food_name, image, price, desc, type_id } = req.body;
+  await prisma.food.create({
+    data: { food_name, image, price, desc, type_id },
+  });
+  await prisma.food.update({
+    data: { food_name, image, price, desc, type_id },
+    where: { food_id: 1 },
+  });
+  await prisma.food.delete({where:{food_id:1}});
+});
